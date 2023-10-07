9de469afbef3309b0eb4baa502869ce87eef535f
Jonas Mukuri Maka <zmukuri@gmail.com>
2023-10-07 13:56:40 +0200

    Phase 8: ORM (Affichage en colonne des infos provenant de la BDD).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -10,17 +10,39 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\VisiteRepository;
 /**
  * Description of VoyagesController
  *
  * @author zmuku
  */
 class VoyagesController extends AbstractController {
+
+    private $VisiteRepository;
+
     /**
      * @Route("/voyages", name="voyages")
      * @return Response
      */
     public function index(): Response{
-        return $this->render("pages/voyages.html.twig");
+        $visites = $this->repository->findAll();
+        return $this->render("pages/voyages.html.twig", 
+            ['visites' => $visites
+        ]);
+        
+    }
+    /**
+     * 
+     * @var VisiteRepository
+     */
+    private $repository;
+    
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    public function __construct(VisiteRepository $repository) {
+        
+        $this->repository = $repository;
     }
 }
